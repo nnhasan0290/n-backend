9e34a0b415ead4597015a056c8fd6dbd17bbe47b
nazmul <nazmulhasan02901@gmail.com>
2022-09-01 14:29:59 +0600

    post

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,12 +1,15 @@
 import express from "express";
-import { loadUser, loginUser,logoutUser } from "../Controllers/userController.js";
+import {
+  loadUser,
+  loginUser,
+  logoutUser,
+} from "../Controllers/userController.js";
 import { createUser } from "../Controllers/userController.js";
 const router = express.Router();
 
 router.route("/user/creating").post(createUser);
 router.route("/user/login").post(loginUser);
-router.route("/user/logout").get(logoutUser);
+router.route("/user/logout").post(logoutUser);
 router.route("/user/load").get(loadUser);
-
 
 export default router;
