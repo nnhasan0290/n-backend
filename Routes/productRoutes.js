12d3bbe0d3fdf1d12145d3ab117a070c8b741ec5
nazmul <nazmulhasan02901@gmail.com>
2022-09-05 06:51:05 +0600

    create

--- Routes/productRoutes.js
+++ Routes/productRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { createNewReview, createProduct, getSingleProduct } from "../Controllers/productController.js";
-import {getAllProducts} from "../Controllers/productController.js";
+import {
+  createNewReview,
+  createProduct,
+  getSingleProduct,
+} from "../Controllers/productController.js";
+import { getAllProducts } from "../Controllers/productController.js";
 import isAuthencicated from "../middlewars/auth.js";
 const router = express.Router();
 
-router.route("/product/create").post(isAuthencicated,createProduct);
+router.route("/product/create").post(createProduct);
 router.route("/products").get(getAllProducts);
 router.route("/product/newreview").put(createNewReview);
 router.route("/product/singleproduct").get(getSingleProduct);
